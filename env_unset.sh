949285ccfcaae0340ec3f2dc7eda52640029690c
tianduanrui <2407223896@qq.com>
2017-10-13 09:29:42 +0800

    my build config

---
+++ env_unset.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+export PATH=$(echo ${PATH//\/mnt\/hgfs\/abel\/Develop\/b0-toolskits\/compiler\/CMake.app\/Contents\/MacOS:/})
+export PATH=$(echo ${PATH//\/mnt\/hgfs\/abel\/Develop\/b0-toolskits\/linuxLibraries\/Qt\/5.9.1\/android_x86\/bin:/})
+export PATH=$(echo ${PATH//\/mnt\/hgfs\/abel\/Develop\/b0-toolskits\/linuxAndroidLibraries\/Java\/jdk1.8.0_144\/bin:/})
+export PATH=$(echo ${PATH//\/mnt\/hgfs\/abel\/Develop\/b0-toolskits\/linuxAndroidLibraries\/android-sdk-macosx:/})
+export PATH=$(echo ${PATH//\/mnt\/hgfs\/abel\/Develop\/b0-toolskits\/linuxAndroidLibraries\/android-sdk-macosx\/platform-tools:/})
+export PATH=$(echo ${PATH//\/mnt\/hgfs\/abel\/Develop\/b0-toolskits\/linuxAndroidLibraries\/android-sdk-macosx\/build-tools:/})
+export PATH=$(echo ${PATH//\/mnt\/hgfs\/abel\/Develop\/b0-toolskits\/linuxAndroidLibraries\/android-sdk-macosx\/tools:/})
+export PATH=$(echo ${PATH//\/mnt\/hgfs\/abel\/Develop\/b0-toolskits\/linuxAndroidLibraries\/apache-ant-1.10.1\/bin:/})
+export PATH=$(echo ${PATH//\/mnt\/hgfs\/abel\/Develop\/b0-toolskits\/linuxAndroidLibraries\/android-ndk-r13b\/toolchains\/x86-4.9\/prebuilt\/linux-x86_64\/bin:/})
+unset CLICOLOR
+unset ANDROID_API_VERSION
+unset ANDROID_SDK_ROOT
+unset ANDROID_NDK_ROOT
+unset ANDROID_NDK_HOST
+unset ANDROID_NDK_TOOLCHAIN_PREFIX
+unset ANDROID_NDK_TOOLCHAIN_VERSION
+unset ANDROID_NDK_PLATFORM
+unset NDK_TOOLCHAIN_PATH
+unset NDK_TOOLS_PREFIX
+unset PYCMD_MYNAME
+unset a_special_var_const
+unset QKIT
+unset QSYS
+unset QTDIR
+unset QTSPEC
+unset QTCONFIG
+unset JAVA_HOME
+unset CLASSPATH
